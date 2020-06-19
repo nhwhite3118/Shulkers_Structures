776df3c36b11757e94339586dcfcaa3705b8972d
Nick White <nhwhite3118@gmail.com>
2020-06-19 05:57:17 -0500

    Fixed swamp huts being way too common. I think it's spawning at a rate of 1 per 150 swamp chunks now

--- src/main/java/com/nhwhite3118/structures/swamphut/SwampHut.java
+++ src/main/java/com/nhwhite3118/structures/swamphut/SwampHut.java
@@ -30,44 +30,23 @@ public class SwampHut extends Feature<NoFeatureConfig> {
 
     @Override
     public boolean place(IWorld world, ChunkGenerator<? extends GenerationSettings> changedBlock, Random rand, BlockPos position, NoFeatureConfig p_212245_5_) {
+        if (rand.nextInt(150) != 0) {
+            return false;
+        }
         Rotation rotation = Rotation.randomRotation(rand);
         BlockPos.Mutable blockpos$Mutable = new BlockPos.Mutable();
         BlockPos.Mutable blockpos$MutableOffsetVector = new BlockPos.Mutable();
 
-        // I don't care anymore about compatibility, it's late and I want this working before I go to sleep
-//        int y = world.getHeight(Heightmap.Type.WORLD_SURFACE_WG, position).getY() - 1;
         int y = 62;
         int yDiff = y - position.getY();
 
         boolean validPositionFound = false;
-
-        // This is trying to do it the right way. I'll revisit it when I've had more sleep
-//        for (int checkX = 40; checkX >= -40 && validPositionFound == false; checkX -= 2) {
-//            for (int checkZ = 40; checkZ >= -40 && validPositionFound == false; checkZ -= 2) {
-//                if (world.getBlockState(blockpos$Mutable.setPos(position.add(blockpos$MutableOffsetVector.setPos(checkX, yDiff, checkZ).rotate(rotation))))
-//                        .getBlock() == Blocks.WATER) {
-//                    if (world
-//                            .getBlockState(
-//                                    blockpos$Mutable.setPos(position.add(blockpos$MutableOffsetVector.setPos(checkX - 1, yDiff, checkZ).rotate(rotation))))
-//                            .getBlock() != Blocks.WATER) {
-//                        // we probably hit land. maybe a ship or tree. w/e
-//                        if (outlineFits(world, blockpos$Mutable, rotation)) {
-//                            validPositionFound = true;
-//                        }
-//                    }
-//                }
-//            }
-//        }
         if (world.getBlockState(blockpos$Mutable.setPos(position.getX(), y, position.getZ())).getBlock() == Blocks.WATER) {
             if (world.getBlockState(new BlockPos(position.add(blockpos$MutableOffsetVector.setPos(9, yDiff, 14).rotate(rotation))))
                     .getBlock() == Blocks.WATER) {
-                // we probably hit land. maybe a ship or tree. w/e
-                // if (outlineFits(world, blockpos$Mutable, rotation)) {
                 validPositionFound = true;
-                // }
             }
         }
-        // Checking all those blocks is expensive. We want to avoid ruining performance too badly
         if (validPositionFound == false) {
             return false;
         }
@@ -84,12 +63,4 @@ public class SwampHut extends Feature<NoFeatureConfig> {
 
         return true;
     }
-
-//    private boolean outlineFits(IWorld world, BlockPos positionCandidate, Rotation rotation) {
-//        boolean valid = (world.getBlockState(positionCandidate.add(new BlockPos(9, 0, 0).rotate(rotation))).getBlock() != Blocks.WATER
-//                || world.getBlockState(positionCandidate.add(new BlockPos(8, 0, 0).rotate(rotation))).getBlock() != Blocks.WATER)
-//                && (world.getBlockState(positionCandidate.add(new BlockPos(9, 0, -14).rotate(rotation))).getBlock() == Blocks.WATER
-//                        || world.getBlockState(positionCandidate.add(new BlockPos(8, 0, -13).rotate(rotation))).getBlock() == Blocks.WATER);
-//        return valid;
-//    }
 }
